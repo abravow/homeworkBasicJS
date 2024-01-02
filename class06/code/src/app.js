4f35fc853222b3f2675d6ac751aa457c784df808
abravow <36878704+abravow@users.noreply.github.com>
2024-01-02 18:58:58 +0100

    Add files via upload

---
+++ class06/code/src/app.js
@@ -0,0 +1,7 @@
+let heading=document.getElementById("header");
+let button=document.getElementsByTagName("button")[0];
+
+button.addEventListener("click",function(){
+    heading.innerText="textot nevazi"
+})
+
